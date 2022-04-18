0d071d9b92cf6a3b0fec565990661d0c7f793148
Min Kim <msk@dolbo.net>
2022-04-18 11:33:11 -0700

    Update clap to v3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{Arg, Command};
 use reproduce::{Config, Controller};
 use std::num::{NonZeroI64, NonZeroU8};
 
@@ -18,108 +18,108 @@ pub fn main() {
 #[allow(clippy::too_many_lines)]
 #[must_use]
 pub fn parse() -> Config {
-    let m = App::new("reproduce")
+    let m = Command::new("reproduce")
     .version(env!("CARGO_PKG_VERSION"))
     .arg(
-        Arg::with_name("broker")
-            .short("b")
+        Arg::new("broker")
+            .short('b')
             .takes_value(true)
 	    .value_name("host1:port1,host2:port2,..")
             .help("Kafka broker list"),
     )
     .arg(
-        Arg::with_name("count")
-            .short("c")
+        Arg::new("count")
+            .short('c')
             .takes_value(true)
             .help("Send count"),
     )
     .arg(
-        Arg::with_name("source")
-            .short("d")
+        Arg::new("source")
+            .short('d')
             .takes_value(true)
             .default_value("1")
             .help("Data source ID (1-255)"),
     )
     .arg(
-        Arg::with_name("eval")
-            .short("e")
+        Arg::new("eval")
+            .short('e')
             .help("Evaluation mode. Outputs statistics of transmission.")
     )
     .arg(
-        Arg::with_name("continuous")
-            .short("g")
+        Arg::new("continuous")
+            .short('g')
             .help("Continues to read from a growing input file")
     )
     .arg(
-        Arg::with_name("input")
-            .short("i")
+        Arg::new("input")
+            .short('i')
             .takes_value(true)
             .help("Input [LOGFILE/DIR] \
 	    	   If not given, internal sample data will be used.")
     )
     .arg(
-        Arg::with_name("seq")
-            .short("j")
+        Arg::new("seq")
+            .short('j')
             .takes_value(true)
             .help("Sets the initial sequence number (0-16777215).")
     )
     .arg(
-        Arg::with_name("pattern")
-            .short("m")
+        Arg::new("pattern")
+            .short('m')
             .takes_value(true)
             .value_name("FILE")
             .help("Pattern filename")
     )
     .arg(
-        Arg::with_name("prefix")
-            .short("n")
+        Arg::new("prefix")
+            .short('n')
             .takes_value(true)
             .help("Prefix of file names to send multiple files or a directory")
     )
     .arg(
-        Arg::with_name("output")
-            .short("o")
+        Arg::new("output")
+            .short('o')
             .takes_value(true)
             .help("Output type [TEXTFILE/none]. \
                    If not given, the output is sent to Kafka.")
     )
     .arg(
-        Arg::with_name("period")
-            .short("p")
+        Arg::new("period")
+            .short('p')
             .takes_value(true)
             .default_value("3")
             .help("Sepcifies how long data may be kept in the queue.")
     )
     .arg(
-        Arg::with_name("size")
-            .short("q")
+        Arg::new("size")
+            .short('q')
             .takes_value(true)
             .default_value("900000")
             .help("Specifies the maximum number of bytes to be sent to Kafka in a single message.")
     )
     .arg(
-        Arg::with_name("offset")
-            .short("r")
+        Arg::new("offset")
+            .short('r')
             .takes_value(true)
             .help("Record (prefix of offset file). Using this option will start the conversation \
                    after the previous conversation. The offset file name is managed by \
                    <input_file>_<prefix>.")
     )
     .arg(
-        Arg::with_name("skip")
-            .short("s")
+        Arg::new("skip")
+            .short('s')
             .takes_value(true)
             .help("Skip count")
     )
     .arg(
-        Arg::with_name("topic")
-            .short("t")
+        Arg::new("topic")
+            .short('t')
             .takes_value(true)
             .help("Kafka topic name. The topic should be available on the broker.")
     )
     .arg(
-        Arg::with_name("polling")
-            .short("v")
+        Arg::new("polling")
+            .short('v')
             .help("Polls the input directory")
     )
     .get_matches();
