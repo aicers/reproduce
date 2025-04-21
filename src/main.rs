17308dd13812fd08845f0e1e976c1b5e3d4e0277
Keon <greenare@cluml.com>
2025-04-21 11:29:53 +0900

    Log to file if log_path set, otherwise stdout
    - Removed dependency on build type

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ ARG:
 async fn main() -> Result<()> {
     let config_filename = parse();
     let config = Config::new(config_filename.as_ref())?;
-    let _guards = init_tracing(config.log_path.as_deref())?;
+    let _guard = init_tracing(config.log_path.as_deref())?;
     let controller = Controller::new(config);
     info!("reproduce start");
     let _handle = task::spawn(async move {
@@ -85,25 +85,22 @@ fn version() -> String {
     format!("REproduce {}", env!("CARGO_PKG_VERSION"))
 }
 
-/// Initializes the tracing subscriber and returns a vector of `WorkerGuard` that flushes the log
-/// when dropped.
+/// Initializes the tracing subscriber and returns a `WorkerGuard` that flushes the log when
+/// dropped.
 ///
 /// If `log_path` is `None`, logs will be printed to stdout.
 ///
 /// If the runtime is in debug mode, logs will be printed to stdout in addition to the specified
 /// `log_path`.
-fn init_tracing(log_path: Option<&Path>) -> Result<Vec<WorkerGuard>> {
-    let mut guards = vec![];
-
-    let file_layer = if let Some(log_path) = log_path {
+fn init_tracing(log_path: Option<&Path>) -> Result<WorkerGuard> {
+    let (layer, guard) = if let Some(log_path) = log_path {
         let file = OpenOptions::new()
             .create(true)
             .append(true)
             .open(log_path)
             .with_context(|| format!("Failed to open the log file: {}", log_path.display()))?;
         let (non_blocking, file_guard) = tracing_appender::non_blocking(file);
-        guards.push(file_guard);
-        Some(
+        (
             fmt::Layer::default()
                 .with_ansi(false)
                 .with_target(false)
@@ -113,28 +110,20 @@ fn init_tracing(log_path: Option<&Path>) -> Result<Vec<WorkerGuard>> {
                         .with_default_directive(LevelFilter::INFO.into())
                         .from_env_lossy(),
                 ),
+            file_guard,
         )
     } else {
-        None
-    };
-
-    let stdout_layer = if file_layer.is_none() || cfg!(debug_assertions) {
         let (stdout_writer, stdout_guard) = tracing_appender::non_blocking(std::io::stdout());
-        guards.push(stdout_guard);
-        Some(
+        (
             fmt::Layer::default()
                 .with_ansi(true)
                 .with_writer(stdout_writer)
                 .with_filter(EnvFilter::from_default_env()),
+            stdout_guard,
         )
-    } else {
-        None
     };
 
-    tracing_subscriber::Registry::default()
-        .with(stdout_layer)
-        .with(file_layer)
-        .init();
+    tracing_subscriber::Registry::default().with(layer).init();
     info!("Initialized tracing logger");
-    Ok(guards)
+    Ok(guard)
 }
